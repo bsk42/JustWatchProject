9963e443e043590c025d44e8372fcb45b911740e
Benjamin Kaufman <bsk42@wharton.upenn.edu>
2022-04-10 15:20:11 -0400

    editing movie selection screen

--- justwatch/src/Screens/Movieselection.js
+++ justwatch/src/Screens/Movieselection.js
@@ -1,28 +1,24 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import './Movieselection.css';
 
 function Movieselection() {
+
   const likeMovie = null;
   const dislikeMovie = null;
   const superlikeMovie = null;
-  const ReturnButton = null;
-  const GoToProfile = null;
-  const GoToGroups = null;
+
+  const [movieTitle, setMovieTitle] = useState("Borat");
+  const [movieDescription, setMovieDescription] = useState("2006 ‧ Adventure ‧ 1h 24m Kazakh TV talking head Borat /nis dispatched to the United States to report on the greatest /ncountry in the world. With a documentary crew in tow, Borat /nbecomes more interested in locating and marrying Pamela /nAnderson.");
 
   return (
     <div className="div">
-     
       <div className="div-3">
         <div className="builder-columns div-4">
           <div className="builder-column column">
-            <div className="title">Borat</div>
+            <div className="title">{movieTitle}</div>
             <div className="borat-2006-adventure-1-h-24">
-              2006 ‧ Adventure ‧ 1h 24m Kazakh TV talking head Borat
-              is dispatched to the United States to report on the greatest
-              country in the world. With a documentary crew in tow, Borat
-              becomes more interested in locating and marrying Pamela
-              Anderson.
+            {movieDescription}
             </div>
           </div>
         </div>
