841c5ff22458025dab3934484b8aafb5eb6e1cee
Matthew Pearl <pearlm@seas.upenn.edu>
2022-04-29 14:15:56 -0400

    added some routes to fetcher

---
+++ justwatch/src/fetcher.js
@@ -0,0 +1,54 @@
+const hostUrl = 'http://localhost:5005/'
+
+const register = async (username, name, email, password) => {
+  const res = await fetch(`${hostUrl}/register`, {
+    method: 'POST',
+    body: JSON.stringify({
+      username, 
+      name,
+      email,
+      password,
+    }),
+  });
+  return res.json();
+}
+
+const login = async (username, password) => {
+  const res = await fetch(`${hostUrl}/login?username=${username}&password=${password}`, {
+    method: 'GET',
+  });
+  return res.json();
+}
+
+const getMovies = async () => {
+  const res = await fetch(`${hostUrl}/movies`, {
+    method: 'GET',
+  });
+  return res.json();
+}
+
+const deleteUser = async (player) => {
+  const res = await fetch(`${hostUrl}/delete?player=${player}`, {
+    method: 'DELETE'
+  });
+  return res.json();
+}
+
+const updateScore = async (player, points) => {
+  const res = await fetch(`${hostUrl}/leaders`, {
+    method: 'POST',
+    body: JSON.stringify({
+      player,
+      points,
+    }),
+  });
+  return res.json();
+}
+
+export {
+  register,
+  login,
+  getMovies,
+  deleteUser,
+  updateScore,
+}
