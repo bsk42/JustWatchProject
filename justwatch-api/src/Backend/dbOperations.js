689b5ddd5b4a07d5edfe8e23bad27241eb69e58b
alexruhl11 <aruhl@seas.upenn.edu>
2022-05-07 16:56:04 -0400

    updates

--- justwatch-api/src/Backend/dbOperations.js
+++ justwatch-api/src/Backend/dbOperations.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 // 1. Import MongoDB driver
-const { waitFor } = require('@testing-library/react');
 const { MongoClient } = require('mongodb');
 
 // 2. Connect to the DB and return the connection object
