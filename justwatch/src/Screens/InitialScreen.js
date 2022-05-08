311c167939898307d85b107802397a863a3f2114
alexruhl11 <aruhl@seas.upenn.edu>
2022-05-07 22:42:59 -0400

    blob is gone
    blob is gone!

--- justwatch/src/Screens/InitialScreen.js
+++ justwatch/src/Screens/InitialScreen.js
@@ -15,13 +15,6 @@ function InitialScreen() {
 
   return (
     <div className="div">
-      <div className="div-2">
-        <div className="builder-columns div-3">
-          <div className="builder-column column">
-            <div className="div-4">Just Watch</div>
-          </div>
-        </div>
-      </div>
       <button className="button" type="submit" onClick={handleLogin}>LOGIN</button>
       <button className="button" type="submit" onClick={handleRegister}>REGISTER</button>
     </div>
