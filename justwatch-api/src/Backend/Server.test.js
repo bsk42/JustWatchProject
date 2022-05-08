89b1b219edb27ffae68b7d61c09e3c78d4fd6f9d
alexruhl11 <aruhl@seas.upenn.edu>
2022-05-07 21:30:26 -0400

    server tests

--- justwatch-api/src/Backend/Server.test.js
+++ justwatch-api/src/Backend/Server.test.js
@@ -59,77 +59,33 @@ afterAll(async () => {
     dbModule.deleteUser(db, 'user2');
 });
 
-describe('/login endpoint tests',  ()=> {
-    test('/login endpoint status code and response 404', ()=>{
-        //construct a supertest request with our app
-        // send an HTTP POST request with data (body)
-        return request(webapp).post('/login/')
-        .send({player:'', points:3}).expect(404)
-        .then((response)=> expect(JSON.parse(response.text).error).toBe('username not provided'));
+describe('/register endpoint tests',  ()=> {
+    test('/register endpoint status code and response 404', ()=>{
+        return request(webapp).post('/register')
+        .send({player:'', points:3}).expect(500)
+        .then((response)=> expect(JSON.parse(response.text).error).toBe('try again later'));
     });
 
      test('status code 201 and response', () =>{
-        return request(webapp).post('/login/')
+        return request(webapp).post('/register')
         .send(player)
         .expect(201) // test the response status code
          // process the response
-        .then((response)=> expect(JSON.parse(response.text).message).toContain('Player with id'));
+        .then((response)=> expect(JSON.parse(response.text).message).toContain('Player with username'));
     }); 
 });
 
-describe('/leaders get endpoint tests',  ()=> {
+
+describe('/movies endpoint tests',  ()=> {
+    test('/movies endpoint status code and response 500', ()=>{
+        return request(webapp).get('/movies/:id')
+        .expect(500)
+        .then((response)=> expect(JSON.parse(response.text).error).toBe('error retrieving movie'));
+    });
+
      test('status code 200 and response', () =>{
-        return request(webapp).get('/leaders/5')
+        return request(webapp).get('/movies')
         .expect(200) // test the response status code
-         // process the response
-        .then((response)=> expect(JSON.parse(response.text).message).toContain('alex'));
+        .then((response)=> expect(JSON.parse(response.text).message).toContain('tt8097030'));
     }); 
 });
-
-describe('/leaders put endpoint tests',  ()=> {
-    test('status code 201 and response', () =>{
-        return request(webapp).post('/login/')
-        .send(playerToUpdate)
-        .expect(201) // test the response status code
-         // process the response
-        .then((response)=> expect(JSON.parse(response.text).message).toContain('Player with id'));
-    }); 
-    test('status code 200 and response', () =>{
-        return request(webapp).put('/leaders/')
-        .send({player: 'Update2', points: 10})
-        .expect(200) // test the response status code
-        // process the response
-        .then((response)=> expect(JSON.parse(response.text).message).toContain('10'));
-   }); 
-   test('status code 200 and response', () =>{
-    return request(webapp).delete('/delete/Update2')
-        .expect(200) // test the response status code
-         // process the response
-        .then((response)=> expect(true));
-}); 
-});
-
-describe('/quiz endpoint tests',  ()=> {
-    test('status code 200 and response', () =>{
-       return request(webapp).get('/quiz/')
-       .expect(200) // test the response status code
-        // process the response
-       .then((response)=> expect(JSON.parse(response.text).message).toContain('Margot Robbie'));
-   }); 
-});
-
-
-describe('/delete endpoint tests',  ()=> {
-     test('status code 200 and response', () =>{
-        request(webapp).post('/login/')
-        .send(playerToDelete);
-        return request(webapp).delete('/delete/delete2')
-        .expect(200) // test the response status code
-         // process the response
-        .then((response)=> expect(true));
-    }); 
-});
-
-
-
-
